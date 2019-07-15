94996c1f9f9d28e195dd5ce5f5444b4f75a295bc
Badri Vasistha <badri.vasistha@gmail.com>
2019-07-14 19:14:53 -0700

    styled with css

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ var mysql = require('mysql');
 var connection;
 
 // //The following code is from the Heroku deployment process instructions for MySQL
-// if (process.env.JAWSDB_URL) {
-// 	// We're on Heroku and will let MySQL's createConnection connect us, using the JawsDB URL that's in our process environment
-// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+	// 	// We're on Heroku and will let MySQL's createConnection connect us, using the JawsDB URL that's in our process environment
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 	// else we are running on our old friend localhost. Use the standard 3306 port
 	connection = mysql.createConnection({
 		port: 3306,
@@ -19,14 +19,14 @@ var connection;
 	});
 
 
-// Make the connection to MySQL
-connection.connect(function(err) {
-  if (err) {
-    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
-    return;
-  }
-  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
-});
+	// Make the connection to MySQL
+	connection.connect(function (err) {
+		if (err) {
+			console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+			return;
+		}
+		console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+	})};
 
-// Export connection for ORM use
-module.exports = connection;
+	// Export connection for ORM use
+	module.exports = connection;
